721519f6a5b2f2bac1370769b2b610183be4a418
Ahmed Samy <as8609377@gmail.com>
2024-12-09 18:42:01 +0200

    Container is finished and section opponent is finished.

---
+++ assets/script/main.js
@@ -0,0 +1,10 @@
+document.getElementById("category").addEventListener("change", function () {
+  const helperText = document.getElementById("helper-text");
+
+  if (this.value) {
+    helperText.style.opacity = "0"; 
+    setTimeout(() => {
+      helperText.style.display = "none"; 
+    }, 300); 
+  }
+});
